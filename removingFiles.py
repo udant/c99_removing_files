27e1602be7b5eafb0de49d947238d299544d55ab
udant <61499772+udant@users.noreply.github.com>
2020-10-22 11:52:02 +0530

    Add files via upload

--- removingFiles.py
+++ removingFiles.py
@@ -3,14 +3,74 @@ import shutil
 import time
 
 def main():
+    deleted_folders_count = 0
+    deleted_files_count = 0
+    path = input("/PATH_TO_DELETE ")
+    days = 0
+    fileorno= input("file or folder ")
+    seconds = time.time() - (days * 24 * 60 * 60)
+    print("TEst program")
+    if os.path.exists(path):
+        print(path+' is  found')
+        for root_folder, folders, files in os.walk(path):
+            #1. name=path+filenme
+            #2. Create file name 
+            #3. calculate ctime 
+            #4. compare ctime
+            #5. 
+           
+            if seconds >= get_file_or_folder_age(root_folder):
+                if fileorno=="file" :
+                    name = input("filename:")
+                    folder=path+"/" + name
+                    remove_file(folder)
+                    deleted_folders_count += 1 
+                elif fileorno=="folder":
+                    remove_folder(path)
+                    deleted_folders_count += 1       
+                            
+    else:
+        print(path+' is not found')
+ 
+
+def remove_folder(path):
+	if not shutil.rmtree(path):
+		print(path+" is removed successfully")
+	else:
+		print("Unable to delete the "+path)
+  
+def remove_file(path):
+	if not os.remove(path):
+     
+		print(path+" is removed successfully")
+	else:
+		print("Unable to delete the "+path)
+
+def get_file_or_folder_age(path):
+	ctime = os.stat(path).st_ctime
+	return ctime
+
+if __name__ == '__main__':
+	main()
+ 
+                                    
+                                    
+ 
+"""
+import os
+import shutil
+import time
+
+def main():
 	deleted_folders_count = 0
 	deleted_files_count = 0
 	path = input("/PATH_TO_DELETE ")
-	days = 30
+	days = 0
 	seconds = time.time() - (days * 24 * 60 * 60)
 
 	if os.path.exists(path):
 		for root_folder, folders, files in os.walk(path):
+            print("root_" + root_folder +"Folder"+ folders + f )
 			if seconds >= get_file_or_folder_age(root_folder):
 				remove_folder(root_folder)
 				deleted_folders_count += 1 
@@ -26,13 +86,12 @@ def main():
 					if seconds >= get_file_or_folder_age(file_path):
 						remove_file(file_path)
 						deleted_files_count += 1 
-		else:
-			if seconds >= get_file_or_folder_age(path):
-				remove_file(path)
-				deleted_files_count += 1 
+                    else:
+                        if seconds >= get_file_or_folder_age(path):
+                            remove_file(path)
+                            deleted_files_count += 1 
 	else:
 		print(path+' is not found')
-		deleted_files_count += 1 
 	print("Total folders deleted: {deleted_folders_count}")
 	print("Total files deleted: {deleted_files_count}")
 
@@ -52,6 +111,5 @@ def get_file_or_folder_age(path):
 	ctime = os.stat(path).st_ctime
 	return ctime
 
-
 if __name__ == '__main__':
-	main()
+	main()"""
